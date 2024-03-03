2e064a7b3c1033600d1f261ea9e480aa24bbeee0
MHernandezOlmo <60235796+MHernandezOlmo@users.noreply.github.com>
2024-03-03 01:28:46 +0100

    Some Relics Updated

---
+++ Assets/Scripts/Relic.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RelicData
+{
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Sprite Icon { get; private set; }
+        public int Rarity { get; private set; }
+}
